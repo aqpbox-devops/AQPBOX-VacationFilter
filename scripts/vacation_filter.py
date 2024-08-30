5c813c9f9c4111397e01ee6eacdcb3c6f20363e2
aqpbox-devops <imamanih@cajaarequipa.pe>
2024-08-30 17:01:00 -0500

    Attachments capture

--- scripts/vacation_filter.py
+++ scripts/vacation_filter.py
@@ -1,33 +1,54 @@
 import exchangelib as exlib
+import pandas as pd
 from dotenv import load_dotenv
+from constants import *
+
 import os
+import io
 
 def clean_lna(text):
     ls = str(text).splitlines()
     filtered = [l for l in ls if l.strip() != '']
     return '[&&]'.join(filtered)
     
-load_dotenv()
+if __name__ == '__main__':
+    load_dotenv()
 
-email = os.getenv('CAJAAQP_EMAIL')
-password = os.getenv('CAJAAQP_PASSWORD')
-email_dir = os.getenv('EMAIL_DIRECTORY_READ')
+    email = os.getenv('CAJAAQP_EMAIL')
+    password = os.getenv('CAJAAQP_PASSWORD')
+    vacati_dir = os.getenv('VACATIONS_DIR')
+    dataub_dir = os.getenv('DATAUB_DIR')
 
-print(email, '*'*len(password), email_dir, sep='|')
+    credentials = exlib.Credentials(email, password)
+    account = exlib.Account(email, credentials=credentials, autodiscover=True)
 
-credentials = exlib.Credentials(email, password)
-account = exlib.Account(email, credentials=credentials, autodiscover=True)
+    inbox = account.inbox
 
-inbox = account.inbox
-folder = inbox / email_dir
+    dataub_df = None
+    for item in (inbox / dataub_dir).all().order_by('-datetime_received')[:1]:
+        if isinstance(item, exlib.Message):
+            for attachment in item.attachments:
+                if attachment.name.endswith('.xlsx') and attachment.name.startswith("Data ubicación"):
+                    with io.BytesIO(attachment.content) as file:
+                        dataub_df = pd.read_excel(file)
 
-for item in folder.all():#filter(is_read=False):
-    if isinstance(item, exlib.Message):
-        print(f"[ID:{item.conversation_id}]")
-        print(f"\tAsunto: {item.subject}")
-        print(f"\tDe: {item.sender.email_address}")
-        print(f"\tFecha de recibo: {item.datetime_received}")
-       # print(f"\tXML: {item.to_xml}")
-        print(f"\tCuerpo: {clean_lna(item.text_body)[:100]}")
-        print('*'*200)
-        item.is_read = True
+    recv_emails = {}
+
+    for item in (inbox / vacati_dir).all().order_by('-datetime_received'):
+        if isinstance(item, exlib.Message):
+            recv_emails[str(item.id)] = {
+                'Subject': item.subject,
+                'From': item.sender.email_address,
+                'Recv. Date': item.datetime_received
+            }
+            item.is_read = True
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+
+    print(recv_emails)
+
+    filtered_df = dataub_df[dataub_df[CKEY_EMAIL].isin({email_data['From'] for email_data in recv_emails.values()})]
+    #dataub_df[CKEY_USERNAME] = dataub_df[CKEY_EMAIL].apply(lambda x: str(x).split('@')[0].lower())
+    print(filtered_df[[CKEY_EMAIL, CKEY_EMPID]].head(15))
